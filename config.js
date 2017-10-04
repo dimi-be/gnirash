b956f5c3002667ab6f0167d080e1a247eda7e7bd
Dimitri Michaux <dimitri.michaux@gmail.com>
2017-10-04 11:55:00 +0200

    Added config + add title to locals

---
+++ config.js
@@ -0,0 +1,9 @@
+const path = require('path')
+
+module.exports = {
+  siteTitle: 'gnirash',
+  sharedFolders: [
+    path.join(__dirname, 'test-folder'),
+  ],
+  secret: "We can't stop here! This is bat country!!",
+}
--- infrastructure/server.js
+++ infrastructure/server.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const config = require('../config')
 const logger = require('../infrastructure/logger')
 const filesRoutes = require('../files/routes')
 
 logger.info('Setting up express')
 
 const app = express()
+
+const locals = {
+  siteTitle: config.siteTitle,
+}
+
+Object.assign(app.locals, locals)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
